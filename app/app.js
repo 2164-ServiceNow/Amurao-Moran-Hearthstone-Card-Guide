3d4ce55266c4381b05968638636413685b3fe6fd
Matt A <Zoxpler@gmail.com>
2025-01-15 12:45:28 -0700

    Add initial web app state
    Loads default search results
    WE GOT IMAGES!!!!😎👍👍👍

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 "use strict";
 
-let app = angular.module("hearthstoneCardGuideApp",["ngRoute"]);
+let app = angular.module("hearthstoneCardGuideApp",["ngRoute", "defaultSearch"]);
 
 app.config(function($locationProvider, $routeProvider)
 {
     $routeProvider
     .when("/", {
-        redirectTo: "/search"
+        // Changed temporarily because live-server keeps crashing the app >:(
+        // redirectTo: "/search"
+        templateUrl: "app/views/search"
     })
     .when("/search", {
         templateUrl: "app/views/search"
@@ -25,33 +27,32 @@ app.config(function($locationProvider, $routeProvider)
         requireBase:false
     });
 })
-  
 
 
-// let apiLink = `https://us.api.blizzard.com/hearthstone/`;
-
-// // Retrieve Access Token
-// let params = new URLSearchParams();
-// params.append("client_id", `12e8413f3f344256a984d99529f986cb`);
-// params.append("client_secret", `E16Mu4edINRoaKjVEk5VrX0ntkOiCQ1d`);
-// params.append("grant_type", "client_credentials");
-// fetch(`https://oauth.battle.net/token`, {
-//     method: "POST",
-//     body: params
-// }).then(response => {
-//     if(response.ok)
-//     {
-//         response.json().then(content => {
-//             localStorage.setItem("token", content["access_token"])
-//             console.log(localStorage.getItem("token"));
-//         })
-//     }
-//     else
-//     {
-//         error("Response not ok")
-//     }
-// });
-// // Retrieve Access Token End
+// TODO: Change to Angular fetch
+// Retrieve Access Token
+let apiLink = `https://us.api.blizzard.com/hearthstone/`;
+let params = new URLSearchParams();
+params.append("client_id", `12e8413f3f344256a984d99529f986cb`);
+params.append("client_secret", `E16Mu4edINRoaKjVEk5VrX0ntkOiCQ1d`);
+params.append("grant_type", "client_credentials");
+fetch(`https://oauth.battle.net/token`, {
+    method: "POST",
+    body: params
+}).then(response => {
+    if(response.ok)
+    {
+        response.json().then(content => {
+            localStorage.setItem("token", content["access_token"])
+            console.log(localStorage.getItem("token"));
+        })
+    }
+    else
+    {
+        error("Response not ok")
+    }
+});
+// Retrieve Access Token End
 
 // // Retrieve Test Card
 // const testCard = 678;
@@ -72,15 +73,3 @@ app.config(function($locationProvider, $routeProvider)
 //     }
 // });
 // // Retrieve Test Card End
-
-// app.config(function($routeProvider, $locationProvider){
-//     $routeProvider
-//     .when("/", {
-//         templateUrl: "app/pages/main.html"
-//     })
-
-//     $locationProvider.html5Mode({
-//         enabled: true,
-//         requireBase: false
-//     })
-// })
---
+++ app/components/defaultSearch/defaultSearch.js
@@ -0,0 +1,24 @@
+let module = angular.module("defaultSearch", []);
+module.component("defaultresults", {
+    template:
+    `<span ng-repeat="card in $ctrl.cards">
+        <img src="{{card.imgsrc}}"/>
+    </span>`,
+    controller: function ($http){
+        $http.get(apiLink + `cards?locale=en_US`, {headers: {"Authorization": "Bearer " + localStorage.getItem("token")}})
+        .then((response) => {
+            console.log(response);
+            if(response.status >= 200 && response.status < 300)
+            {
+                this.cards = [];
+                response.data["cards"].forEach(card => {
+                    this.cards.push({"imgsrc":card["image"]}); 
+                });
+            }
+            else
+            {
+               console.log("API sad: " + response.statusText);
+            }
+        })
+    }
+})
